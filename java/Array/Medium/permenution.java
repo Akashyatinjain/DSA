fef56d6458ed6acc6bfad1094d1a58495e195ee5
Akash-Jain <aj0881871@gmail.com>
2026-01-12 21:33:08 +0530

    permutation

--- java/Array/Medium/permenution.java
+++ java/Array/Medium/permenution.java
@@ -1,27 +1,67 @@
 
-import java.util.*;
-
+// import java.util.*;
+// class Solution {
+//     // Function to find the next permutation
+//     public List<Integer> nextPermutation(int[] nums) {
+//         // List to hold all permutations
+//         List<List<Integer>> all = new ArrayList<>();
+//         // Sort the input to start from the smallest
+//         Arrays.sort(nums);
+//         // Generate all permutations
+//         permute(nums, 0, all);
+//         // Convert array to list for comparison
+//         List<Integer> current = new ArrayList<>();
+//         for (int num : nums) {
+//             current.add(num);
+//         }
+//         // Find and return the next permutation
+//         for (int i = 0; i < all.size(); i++) {
+//             if (all.get(i).equals(current)) {
+//                 if (i == all.size() - 1) {
+//                     return all.get(0);
+//                 }
+//                 return all.get(i + 1);
+//             }
+//         }
+//         return current;
+//     }
+//     // Backtracking permutation generator
+//     private void permute(int[] nums, int start, List<List<Integer>> all) {
+//         if (start == nums.length) {
+//             List<Integer> temp = new ArrayList<>();
+//             for (int num : nums) {
+//                 temp.add(num);
+//             }
+//             all.add(new ArrayList<>(temp));
+//             return;
+//         }
+//         for (int i = start; i < nums.length; i++) {
+//             swap(nums, i, start);
+//             permute(nums, start + 1, all);
+//             swap(nums, i, start);
+//         }
+//     }
+//     // Swap helper
+//     private void swap(int[] arr, int i, int j) {
+//         int tmp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = tmp;
+//     }
+// }
+// public class permenution {
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         int[] nums = {1, 2, 3};
+//         List<Integer> result = sol.nextPermutation(nums);
+//         for (int x : result) {
+//             System.out.print(x + " ");
+//         }
+//         System.out.println();
+//     }
+// }
 public class permenution {
 
     public static void main(String[] args) {
-        int[] nums = {1, 3, 2};
-        Solution sc = new Solution();
-        List<Integer> res = sc.perCom(nums);
-        for (int no : res) {
-            System.err.println(no + " ");
-        }
-    }
-}
 
-class Solution {
-
-    public List<Integer> perCom(int[] nums) {
-        List<List<Integer>> all = new ArrayList<>();
-        Arrays.sort(nums);
-        permute(nums, 0, all);
-    }
-
-    public void permute(int[] nums, int start, List<List<Integer>> all) {
-        if ()
     }
 }
