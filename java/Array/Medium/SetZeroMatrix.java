97d0beef278f7cf12bbef9ecc4067eea7c78f426
Akash-Jain <aj0881871@gmail.com>
2026-02-23 19:40:53 +0530

    set matrix zero full solution

--- java/Array/Medium/SetZeroMatrix.java
+++ java/Array/Medium/SetZeroMatrix.java
@@ -44,28 +44,79 @@ class Solution {
     //     }
     //     return arr;
     // }
-    public int[][] SetZero(int arr[][]) {
-        int m = arr.length;  // row
-        int n = arr[0].length; // columns
+    // public int[][] SetZero(int arr[][]) {
+    //     int m = arr.length;  // row
+    //     int n = arr[0].length; // columns
+    //     boolean[] row = new boolean[m];
+    //     boolean[] col = new boolean[n];
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (arr[i][j] == 0) {
+    //                 row[i] = true;
+    //                 col[j] = true;
+    //             }
+    //         }
+    //     }
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (row[i] || col[j]) {
+    //                 arr[i][j] = 0;
+    //             }
+    //         }
+    //     }
+    //     return arr;
+    // }
+    public int[][] SetZero(int matrix[][]) {
 
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+        boolean firstRow = false;
+        boolean firstCol = false;
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    arr[i][j] = 0;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        return arr;
+
+        if (firstRow) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero the first column if needed
+        if (firstCol) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        return matrix;
     }
 }
