75495b577aab351f1049bdf0a3ee2d0e011d3e74
Akashjain <aj0881871@gmail.com>
2026-06-02 00:24:08 +0530

    adding the majority element code

---
+++ java/Array/HARD/MajorityElements.java
@@ -0,0 +1,26 @@
+
+class Solution {
+
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        int n = nums.length;
+
+        int var = n / 3;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    cnt++;
+                }
+            }
+
+            if (cnt > var && !arr.contains(nums[i])) {
+                arr.add(nums[i]);
+            }
+        }
+
+        return arr;
+    }
+}
