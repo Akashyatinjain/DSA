3c617b99bd48ba7f299dfa8b86a0c93d9be43e9b
Akashjain <aj0881871@gmail.com>
2026-06-01 23:27:39 +0530

    adding the extra bubble sort code

--- java/Sortings/Bubblesort.java
+++ java/Sortings/Bubblesort.java
@@ -23,7 +23,24 @@ class Bubblesort {
                 System.err.println("\n--------------------");
             }
         }
-
+// or 
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1 - i; j++) {
+                p1 = arr[j];
+                p2 = arr[j + 1];
+                if (p1 > p2) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+                System.err.println("After comparing index " + j + " and " + (j + 1));
+                System.err.println("Array now:");
+                for (int k = 0; k < n; k++) {
+                    System.err.print(arr[k] + " ");
+                }
+                System.err.println("\n--------------------");
+            }
+        }
         System.err.println("The array elements are :");
         for (int i = 0; i <= arr.length - 1; i++) {
             System.err.println(arr[i] + " ");
