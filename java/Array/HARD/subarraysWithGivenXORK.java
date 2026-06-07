ee7f8a10cba0923e7beb8da37cbb08cb870e0b57
Akashjain <aj0881871@gmail.com>
2026-06-07 10:54:52 +0530

    adding the hard question3.0

--- java/Array/HARD/subarraysWithGivenXORK.java
+++ java/Array/HARD/subarraysWithGivenXORK.java
@@ -1,21 +1,37 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
 class subarraysWithGivenXORK {
 
     public static void main(String[] args) {
         int arr[] = {4, 2, 2, 6, 4};
         int k = 6;
         int n = arr.length;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            int xor = 0;
-            for (int j = i; j < n; j++) {
-                xor = xor ^ arr[j];
-                if (xor == k) {
-                    cnt++;
-                }
-            }
-        }
-        System.err.println(cnt);
+        // int cnt = 0;
+        // for (int i = 0; i < n; i++) {
+        //     int xor = 0;
+        //     for (int j = i; j < n; j++) {
+        //         xor = xor ^ arr[j];
+        //         if (xor == k) {
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // System.err.println(cnt);
 
+        Map<Integer, Integer> mpp = new HashMap<>();
+        mpp.put(0, 1);
+        int crtPrefix = 0;
+        int cnt = 0;
+        for (int num : arr) {
+            crtPrefix ^= num;
+            int target = crtPrefix ^ k;
+            if (mpp.containsKey(target)) {
+                cnt = cnt + mpp.get(target);
+            }
+            mpp.put(crtPrefix, mpp.getOrDefault(crtPrefix, 0) + 1);
+        }
+        System.err.println(mpp.size());
     }
 }
