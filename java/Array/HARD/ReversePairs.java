6ce95046d8c92a7975203adeb168d7890d64422d
Akashjain <aj0881871@gmail.com>
2026-06-14 11:23:57 +0530

    adding the last wuestion of array

--- java/Array/HARD/ReversePairs.java
+++ java/Array/HARD/ReversePairs.java
@@ -1,17 +1,81 @@
 
+import java.util.ArrayList;
+
 class ReversePairs {
 
     public static void main(String[] args) {
-        int arr[] = {1, 3, 2, 3, 1};
+        // int arr[] = {1, 3, 2, 3, 1};
+        // int n = arr.length;
+        // int cnt = 0;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (arr[i] > 2 * arr[j]) {
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // System.err.println("the count of the number is " + cnt);
+        //--------------------------------------------------------------------
+        //               or 
+        //--------------------------------------------------------------------
+        int arr[] = {4, 1, 2, 3, 1};
         int n = arr.length;
+        int cnt = team(arr, n);
+        System.err.println("the count is :-" + cnt);
+    }
+
+    public static int team(int arr[], int n) {
+        return mergeSort(arr, 0, n - 1);
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] > 2 * arr[j]) {
-                    cnt++;
-                }
+        if (low > high) {
+            return cnt;
+        }
+        int mid = (low + high) / 2;
+        cnt = cnt + mergeSort(arr, low, mid);
+        cnt = cnt + mergeSort(arr, mid + 1, high);
+        cnt = cnt + cntPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return cnt;
+    }
+
+    public static int cntPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] < arr[right]) {
+                right++;
+            }
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+
+    public static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
             }
         }
-        System.err.println("the count of the number is " + cnt);
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
     }
 }
