e39e2035dcee288cccb09285143dbabe0bac6ef1
Akashjain <aj0881871@gmail.com>
2026-06-18 10:50:51 +0530

    adding the binary search 1D

--- java/BinarySearch/1D/UpperLow.java
+++ java/BinarySearch/1D/UpperLow.java
@@ -1,34 +1,29 @@
 
 class UpperLow {
 
-    public static int UpperBound(int[] nums, int x) {
-        int n = nums.length;
-        int var = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= x) {
-                var = i;
+    public int upperBound(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return var;
-        // int low = 0;
-        // int high = n - 1;
-        // int var = n;
-        // while (low <= high) {
-        //     int mid = (low + high) / 2;
-        //     if (nums[mid] > x) {
-        //         var = mid;
-        //         high = mid - 1;
-        //     } else {
-        //         low = mid + 1;
-        //     }
-        // }
-        // return var;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 5, 8, 15, 19};
+        int[] arr = {3, 5, 8, 9, 15, 19};
         int x = 9;
-        int sys = UpperBound(nums, x);
-        System.out.println(sys);
+
+        UpperLow finder = new UpperLow();
+        int ind = finder.upperBound(arr, x);
+        System.out.println("The upper bound is the index: " + ind);
     }
 }
