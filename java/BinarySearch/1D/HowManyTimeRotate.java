e86549372794837b171853e88085d01ff67bb442
Akashjain <aj0881871@gmail.com>
2026-06-23 11:25:12 +0530

    how mant times rotated array

--- java/BinarySearch/1D/HowManyTimeRotate.java
+++ java/BinarySearch/1D/HowManyTimeRotate.java
@@ -3,7 +3,7 @@ class HowManyTimeRotate {
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
-        int minIndex = 0;
+        // int minIndex = 0;
         // int minVal = arr[0];
         // for (int i = 0; i < arr.length; i++) {
         //     if (arr[i] < minVal) {
@@ -15,11 +15,25 @@ class HowManyTimeRotate {
         //----------------------------------------------------------------
         //             OR
         //----------------------------------------------------------------
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                minIndex = i + 1;
+        // for (int i = 0; i < arr.length - 1; i++) {
+        //     if (arr[i] > arr[i + 1]) {
+        //         minIndex = i + 1;
+        //     }
+        // }
+        // System.err.println("Rotated arr times is" + minIndex);
+        //----------------------------------------------------------------
+        //             OR
+        //----------------------------------------------------------------
+        int low = 0;
+        int high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
-        System.err.println("Rotated arr times is" + minIndex);
+        System.err.println("the " + low);
     }
 }
