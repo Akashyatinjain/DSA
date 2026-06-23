1d9e219524b91cd0ca6ae2259f7cf994f2aa88a6
Akashjain <aj0881871@gmail.com>
2026-06-23 11:16:52 +0530

    how many times rotated

---
+++ java/BinarySearch/1D/HowManyTimeRotate.java
@@ -0,0 +1,16 @@
+
+class HowManyTimeRotate {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int minIndex = 0;
+        int minVal = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minVal) {
+                minVal = arr[i];
+                minIndex = i;
+            }
+        }
+        System.err.println("Rotated arr times is" + minIndex);
+    }
+}
