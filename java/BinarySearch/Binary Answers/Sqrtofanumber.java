0274a026dba7befb30a1da7991f1c3825b75f85b
Akashjain <aj0881871@gmail.com>
2026-06-27 11:29:12 +0530

    square root by binary search2

--- java/BinarySearch/Binary Answers/Sqrtofanumber.java
+++ java/BinarySearch/Binary Answers/Sqrtofanumber.java
@@ -4,11 +4,26 @@ class Sqrtofanumber {
     public static void main(String[] args) {
         int n = 28;
         int ans = 0;
-        for (int i = 1; i < n; i++) {
-            if ((long) (i) * i <= n) {
-                ans = i;
+        // for (int i = 1; i < n; i++) {
+        //     if ((long) (i) * i <= n) {
+        //         ans = i;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // System.err.println("the square root is : " + ans);
+        if (n < 2) {
+            ans = 2;
+        }
+        int left = 1;
+        int right = n / 2;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (mid * mid <= n) {
+                ans = (int) mid;
+                left = (int) mid + 1;
             } else {
-                break;
+                right = (int) mid - 1;
             }
         }
         System.err.println("the square root is : " + ans);
