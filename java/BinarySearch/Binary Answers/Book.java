16226647ea8a3b08884a135f5853e72be1e53319
Akashjain <aj0881871@gmail.com>
2026-07-14 20:03:41 +0530

    addinf the book

---
+++ java/BinarySearch/Binary Answers/Book.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+public class Book {
+
+    public static int countStudents(ArrayList<Integer> arr, int pages) {
+        int n = arr.size();
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                pagesStudent += arr.get(i);
+            } else {
+                students++;
+                pagesStudent = arr.get(i);
+            }
+        }
+        return students;
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        if (m > n) {
+            return -1;
+        }
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        for (int i = low; i <= high; i++) {
+            if (countStudents(arr, i) == m) {
+                return i;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n = 5;
+        int m = 4;
+        int ans = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
